c729aa21e412e37025ba0dbb43ab3f74391b49bd
Fletcher LaRue <laruefletcher@gmail.com>
2018-12-19 05:23:05 -0800

    implements shift and unshift to the list object and successfully tests those methods

--- list/__test__/list.test.js
+++ list/__test__/list.test.js
@@ -1,8 +1,9 @@
 'use strict';
 
 // These 2 should be interchangeable!
-const List = require('../list.js');
-// const List = require('../list-constructor.js');
+// const List = require('../list.js');
+const List = require('../list-constructor.js');
+
 
 describe('List Data Structure', () => {
 
@@ -18,6 +19,29 @@ describe('List Data Structure', () => {
     stuff.push('b');
     expect(stuff.length).toEqual(2);
     expect(stuff.data[1]).toEqual('b');
+  });
+
+  it('shifts items off the front', () => {
+    let stuff = new List();
+    stuff.push('a');
+    stuff.push('b');
+    stuff.push('c');
+    stuff.push('d');
+
+    stuff.shift();
+    expect(stuff.length).toEqual(3);
+    expect(stuff.data).toEqual({0:'b', 1:'c', 2:'d'});
+  });
+
+  it('unshifts items to the front', () => {
+    let stuff = new List();
+    stuff.data = { '0': 'a', '1': 'b', '2': 'c' };
+    stuff.length = 3;
+
+    stuff.unshift(['x', 'y', 'z']);
+    
+    expect(stuff.length).toEqual(6);
+    expect(stuff.data).toEqual({0:'x', 1:'y', 2:'z', 3:'a', 4:'b', 5:'c'});
   });
 
 });
--- list/list-constructor.js
+++ list/list-constructor.js
@@ -1,27 +1,68 @@
 'use strict';
 
+let util = require('util');
+
 function List() {
   this.length = 0;
   this.data = {};
 }
-/**
- * Add item to the end of the list
- * @param item
- */
+
+// Add item to the end of the list
 List.prototype.push = function(item) {
   this.data[this.length] = item;
   this.length++;
 };
 
-/**
- * // Remove an item from the end of the list and return it's value
- * @returns {*}
- */
+// Remove an item from the end of the list and return it's value
 List.prototype.pop = function() {
   let returnValue = this.data[this.length];
   delete this.data[this.length];
   this.length--;
   return returnValue;
 };
+
+List.prototype.shift = function() {
+  if (this.length === 0) {
+    return;
+  }
+
+  for (let i = 0; i < this.length-1; i++ ) {
+    this.data[i] = this.data[i+1];
+  }
+  
+  delete this.data[this.length-1];
+  this.length--;
+};
+
+List.prototype.unshift = function(input) {
+
+  let temp = [];
+
+  for (let i = 0; i < this.length; i++){
+    temp[i] = this.data[i];
+  }
+
+  this.data = {};
+  this.length = 0;
+
+  for (let i = 0; i < input.length; i++){
+    this.data[i] = input[i];
+    this.length++;
+  }
+  
+  for (let i = 0; i < temp.length; i++){
+    this.data[this.length] = temp[i];
+    this.length++;
+  }
+};
+
+// .splice(start[, deleteCount[, item1[, item2[, ...]]]])
+// months.splice(4, 1, 'May');
+// replaces 1 element at 4th index
+
+
+
+
+
 
 module.exports = List;
